2f6425f96b93618e148b5f8f89b51a1b95856474
Tyler Tomani <ttomani@gmail.com>
2024-01-01 02:42:09 -0500

    draft1

---
+++ documentation/js/slideShow.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const slideshowContainer = document.querySelector('main');
+    const images = ['/images/airplanSpaceSun.jpg',
+    '/images/CityCloud.jpg',
+    '/images/DimLitMoutainSun.jpg',
+    '/images/Automn_landscape.jpg',
+    '/images/lakeSunset.jpg'
+    ];
+                let currentIndex = 0;
+    slideshowContainer.style.opacity = 1
+    slideshowContainer.style.backgroundImage = images[0]
+    function updateBackground() {
+        // currentIndex = Math.floor(Math.random()* images.length - 1)
+        currentIndex = (currentIndex + 1) % images.length;
+        // console.log(currentIndex)
+        slideshowContainer.style.backgroundImage = 'url(' + images[currentIndex] + ')';
+        slideshowContainer.style.backgroundImage = 'url(' + images[currentIndex] + ')';
+        setTimeout(function () {
+            slideshowContainer.style.opacity = 1;
+        }, 1250);
+    }
+
+    setInterval(updateBackground, 3500); // Change image every 5 seconds
+});
