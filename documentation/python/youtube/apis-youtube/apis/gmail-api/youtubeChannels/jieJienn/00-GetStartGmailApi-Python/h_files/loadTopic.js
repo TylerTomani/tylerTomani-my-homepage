45a2ff2810a5170171d4c8464ba00105a4790008
Tyler Tomani <ttomani@gmail.com>
2024-01-05 01:40:59 -0500

    main

---
+++ documentation/python/youtube/apis-youtube/apis/gmail-api/youtubeChannels/jieJienn/00-GetStartGmailApi-Python/h_files/loadTopic.js
@@ -0,0 +1,16 @@
+const topics = document.querySelectorAll('.topic')
+
+topics.forEach(topic => {
+    topic.addEventListener('click', e => {
+        let a = topic.querySelector('a')
+        open(a.href)
+    })
+    topic.addEventListener('keydown', e => {
+        let key = e.keyCode
+        if(key === 13){
+            let a = topic.querySelector('a')
+            open(a.href,'_self')
+            
+        }
+    })
+})
