947e9b567784c775e88f0d7fbdb77c16c98d596e
Tyler Tomani <ttomani@gmail.com>
2024-01-25 18:25:16 -0500

    main

--- templates/basicPages-templates/sideBar-temp/js/toggleBars.js
+++ templates/basicPages-templates/sideBar-temp/js/toggleBars.js
@@ -8,16 +8,46 @@ navMain.addEventListener('click', e => {
 navMain.addEventListener('keydown', e => {
     let key = e.keyCode
     if(key === 13){   
-        toggleMenu(asideMain)
+        toggleAsideBar(asideMain)
     }
 })
 
 
-function toggleMenu(el){
-    if(!el.classList.contains('hide')){
-        el.classList.add('hide')
-        
+function toggleAsideBar(el){
+    let main = getMain(el.parentElement)
+    let mainContainer = main.querySelector('.main-container')
+    let targetDivContainer = mainContainer.querySelector('.target-div-container')
+    console.log(el)
+    if(!el.classList.contains('hide-aside')){
+        el.classList.add('hide-aside')
+    } else if(el.classList.contains('hide-aside') || el.classList.contains('show-aside')){
+        el.classList.remove('hide-aside')
+        el.classList.remove('show-aside')
     } else {
-        el.classList.remove('hide')
+        el.classList.add('show-aside')
+        setTimeout(()=> {
+            el.classList.remove('hide-aside')
+            el.classList.add('show-aside')
+        },350)
     }
 }   
+
+
+function getMain(parent){
+    if(parent.classList.contains('main')){
+        return parent
+    } else if (parent.parentElement){
+        return getMain(parent.parentElement)
+    } else {
+        return null
+    }
+}
+
+addEventListener('keydown', e => {
+    let key = e.key.toLowerCase()
+    if(key == 'a' || key == 's'){
+        if(asideMain.classList.contains('hide-aside')){
+            asideMain.classList.remove('hide-aside')
+        }
+    }
+});
